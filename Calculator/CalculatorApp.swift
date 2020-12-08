d8e96f1ab7cc9c6f5d05dc0cc391c2b812fbe3b3
OguzYuuksel <74555694+OguzYuuksel@users.noreply.github.com>
2020-12-08 22:30:00 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Oğuz YÜKSEL on 8.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
