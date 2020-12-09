0ee84e313e8d5b20a044ad553d9aa857e7bd5e40
OguzYuuksel <74555694+OguzYuuksel@users.noreply.github.com>
2020-12-09 19:38:26 +0300

    Sadece Iphone 12 için tasarlandı, UI düzenlendi, Basit bir error handling yapıldı proje tamamlandı!

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -15,86 +15,102 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
-            Group{
-            Spacer()
-            Spacer()
-                Spacer()
-            }
-            TextField("Please enter first value...", text: $firstBox)
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 0.2))
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .cornerRadius(12)
-                .font(Font.system(size: 25, weight: .medium, design: .serif))
-                .padding(EdgeInsets(top: 0, leading: 120, bottom: 0, trailing: 120))
-            TextField("Please enter second value...", text: $secondBox)
-                .background(Color.purple)
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 0.2))
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .cornerRadius(12)
-                .font(Font.system(size: 25, weight: .medium, design: .serif))
-                .padding(EdgeInsets(top: 10, leading: 120, bottom: 0, trailing: 120))
-                
-            
-            
-            Spacer()
-            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
-                Spacer()
-                Spacer()
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
                 Group{
-                    Button("+" ,action:  {
-                        result = String(calculator { (a:Double, b:Double) -> Double in
-                            return Double(a) + Double(b)
-                        })
-                    }).foregroundColor(.white).font(.largeTitle)
                     Spacer()
-                    Button("-" ,action:  {
-                        result = String(calculator { (a:Double, b:Double) -> Double in
-                            return Double(a) - Double(b)
-                        })
-                    }).foregroundColor(.white).font(.largeTitle)
                     Spacer()
-                    Button("*" ,action:  {
-                        result = String(calculator { (a:Double, b:Double) -> Double in
-                            return Double(a) * Double(b)
-                        })
-                    }).foregroundColor(.white).font(.largeTitle)
                     Spacer()
-                    Button("/" ,action:  {
-                        result = String(calculator { (a:Double, b:Double) -> Double in
-                            return Double(a) / Double(b)
-                        })
-                    }).foregroundColor(.white).font(.largeTitle)
                 }
+                TextField("Please enter first value...", text: $firstBox)
+                    .frame(height: 40)
+                    .background(Color.white)
+                    .cornerRadius(18)
+                    .font(Font.system(size: 25, weight: .medium, design: .serif))
+                    .padding(EdgeInsets(top: 10, leading: 40, bottom: 0, trailing: 40))
+                TextField("Please enter second value...", text: $secondBox)
+                    .frame(height: 40)
+                    .background(Color.white)
+                    .cornerRadius(18)
+                    .font(Font.system(size: 25, weight: .medium, design: .serif))
+                    .padding(EdgeInsets(top: 10, leading: 40, bottom: 0, trailing: 40))
+                
+                
+                
+                
+                
                 Spacer()
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                    Spacer()
+                    Spacer()
+                    Group{
+                        Button("+" ,action:  {
+                            result = calculator { (a:String, b:String) -> String in
+                                return String (Double(a)! + Double(b)!)
+                            }
+                        }
+                        )
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        Spacer()
+                        Button("-" ,action:  {
+                            result = calculator { (a:String, b:String) -> String in
+                                return String(Double(a)! - Double(b)!)
+                            }
+                        }).foregroundColor(.white).font(.largeTitle)
+                        Spacer()
+                        Button("*" ,action:  {
+                            result = calculator { (a:String, b:String) -> String in
+                                return String(Double(a)! * Double(b)!)
+                            }
+                        }).foregroundColor(.white).font(.largeTitle)
+                        Spacer()
+                        Button("/" ,action:  {
+                            result = calculator { (a:String, b:String) -> String in
+                                return String(Double(a)! / Double(b)!)
+                            }
+                        }).foregroundColor(.white).font(.largeTitle)
+                    }
+                    Spacer()
+                    Spacer()
+                }
+                
+                )
                 Spacer()
-            }
+                Text(result).foregroundColor(.white).font(.largeTitle)
+                Group{
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Text("Developed by OguzYuksel").foregroundColor(.white)
+                    Spacer()
+                }
+            })
             
-            )
-            Spacer()
-            Text(result).foregroundColor(.white).font(.largeTitle)
-            Group{
-            Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-            Spacer()
-            Spacer()
-            Spacer()
-                Text("Developed by OguzYuksel").foregroundColor(.white)
-            Spacer()
-            }
-        })
-        
-        
-        
+            
+            
         }
         
     }
-    func calculator(operation: (Double,Double) -> Double) -> Double {
-        return operation(Double(firstBox) ?? 0, Double(secondBox) ?? 0)
+    
+    func loadNumber(first : String, second: String) -> Bool {
+        guard (Double(first) != nil && Double(second) != nil) else {
+            return false
+        }
+        return true
     }
+    
+    func calculator(operation: (String,String) -> String) -> String {
+        if loadNumber(first: firstBox, second: secondBox) {
+            return operation(firstBox, secondBox)
+        } else
+        {
+            return "Please enter a valid number"
+        }    }
 }
 
 
@@ -102,7 +118,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPad (8th generation)")
+            .previewDevice("iPhone 12 Pro Max")
         
         
     }
